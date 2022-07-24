0e6db6d698f7cf65d951c45eb51aca4a7fb6b7f1
Aurora <jakersaf@gmail.com>
2022-07-24 18:51:19 -0300

    complete exercises 1 and 2
    HOLY MOLY THIS IS FUN

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+  // if (number < 0) {
+  //   return "ERROR";
+  // }
+  // string = string.repeat(number);
+  // return string;
+  if (number < 0) {
+    return "ERROR";
+  }
+  // string = string.repeat(number);
+  let newString = "";
+  for (let i = 0; i < number; i++) {
+    newString += string;
+  }
 
+  return newString;
 };
 
 // Do not edit below this line
