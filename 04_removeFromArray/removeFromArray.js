2f460257048893374261777caf1b49aa368a768b
Aurora <jakersaf@gmail.com>
2022-07-25 20:34:08 -0300

    do exercise 4
    the hardest one so far! i was trying to go for a loop that checks
    every input and exclude the values if they're bigger than the array
    or strings and i almost got it... it was very fun though!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...numToRemove) {
+  for (let i = 0; i < numToRemove.length; i++) {
+    // i was trying to go for a loop but it wasn't quite right :/
+    /* if (typeof numToRemove[i] == "string") {
+      continue;
+    } else if (i > array.lenght) {
+      continue;
+    } else {
+      array.splice(numToRemove[i] - 1, 1);
+    } */
 
+    // serches the index of the value in the array
+    // if the value is greater than 0, remove it from the array
+    let index = array.indexOf(numToRemove[i]);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
