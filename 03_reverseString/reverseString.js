022674475b0329496ae4d30df613a9d07bb6a4bd
Aurora <jakersaf@gmail.com>
2022-07-25 00:37:08 -0300

    complete exercise 3
    omg that was so fucking hard my brain is hurting from thinking
    i could thought of the for loop method but the split reverse join
    is just fucking nuts, completely out of sense, i mean, it does
    make sense i understand how it works but why????????????????

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  // return string.split("").reverse().join(""); // most conventional way
 
+  let newStr = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    newStr += string[i];
+  }
+
+  return newStr;
 };
 
 // Do not edit below this line
