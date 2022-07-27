7d6fcff224bad09d79466401775c8fd28b200cb9
Aurora <jakersaf@gmail.com>
2022-07-27 18:20:22 -0300

    do the converter from farenheit to celsius
    programming is really fun :)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,27 @@
-const ftoc = function() {
+const ftoc = function (tempf) {
+  // round with n.toFixed(1)
+  // but it returns a string
+  // so convert string into number with Number()
 
+  tempf = (tempf - 32) * (5 / 9);
+  tempf = tempf.toFixed(1);
+  tempf = Number(tempf);
+  return tempf;
 };
 
-const ctof = function() {
+const ctof = function (tempc) {
+  // round with n.toFixed(1)
+  // but it returns a string
+  // so convert string into number with Number()
 
+  tempc = tempc * (9 / 5) + 32;
+  tempc = tempc.toFixed(1);
+  tempc = Number(tempc);
+  return tempc;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
