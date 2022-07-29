a5c281e9bdfb089304c1010b3cae605d7cc14bfa
Aurora <jakersaf@gmail.com>
2022-07-29 17:31:27 -0300

    do exer 10
    okay that was like SUPER EASY DUDE WTF

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  // check if it is a negative number
+  if (number < 0) {
+    return "OOPS";
+  }
+  // check if it is a string
+  if (typeof number == "string") {
+    number = parseInt(number);
+  }
 
+  let sequence = [1, 1];
+  // debugging
+  // console.log(`sequence: ${sequence}\nsequenceLength: ${sequence.length}`);
+
+  for (let i = sequence.length; i < number; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+  // debugging
+  // console.log(`sequence: ${sequence}`);
+  // console.log(`sequence -2: ${sequence[sequence.length - 2]}`);
+
+  return sequence[sequence.length - 1];
 };
 
 // Do not edit below this line
