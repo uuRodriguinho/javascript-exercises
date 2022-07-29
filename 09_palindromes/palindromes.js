720e251e59078dcd0cb397778cfab35999d1b83b
Aurora <jakersaf@gmail.com>
2022-07-29 01:42:02 -0300

    have a brain aneurism to do something "simple"
    i just made 2 checks on the test but this will have to be enough
    i need to sleep and every progress is a step forward i think or
    some philosiphycal shit i heard on instagram idk good night

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let newWord = "";
+  let result;
+  let currentWord;
+  let toStrip = "";
 
+  for (let i = word.length - 1; i >= 0; i--) {
+    currentWord = word[i];
+    if (currentWord.toUpperCase() != currentWord.toLowerCase()) {
+      newWord += word[i];
+    } else {
+      toStrip += word[i];
+      continue;
+    }
+  }
+
+  word = word.replace(toStrip, "");
+
+  console.log(`newWord: ${newWord}\ntoSTrip: ${toStrip}\nword: ${word}`);
+
+  if (word === newWord) {
+    result = true;
+  } else {
+    result = false;
+  }
+
+  return result;
 };
+
+console.log(palindromes("racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
